be4e1846fa8e06daa5bdd06ff683c1675a7c3f11
sozarg <mateoterr@gmail.com>
2025-04-28 11:14:48 -0300

    Add settings configuration with Pydantic BaseSettings

---
+++ config.py
@@ -0,0 +1,10 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    XATA_API_KEY: str
+    XATA_BASE_URL: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()

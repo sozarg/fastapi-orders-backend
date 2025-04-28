f9c23f459a1233a9bb164dee923a0f3863bf8f94
sozarg <mateoterr@gmail.com>
2025-04-28 11:39:43 -0300

    Fix XataClient initialization: remove base_url parameter for latest SDK compatibility

--- database/xata.py
+++ database/xata.py
@@ -2,4 +2,4 @@ import os
 from xata import XataClient
 from config import settings
 
-xata_client = XataClient(api_key=settings.XATA_API_KEY, base_url=settings.XATA_BASE_URL)
+xata_client = XataClient(api_key=settings.XATA_API_KEY)
